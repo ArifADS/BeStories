0ac3ff055b8c1dce14234f7868cc744a7de1c924
Arif De Sousa <arifads@gmail.com>
2025-07-07 11:22:41 +0200

    Initial Commit

---
+++ BeStoriesTests/BeStoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeStoriesTests.swift
+//  BeStoriesTests
+//
+//  Created by Arif De Sousa on 7/7/25.
+//
+
+import Testing
+@testable import BeStories
+
+struct BeStoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
